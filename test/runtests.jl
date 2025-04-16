52eeefcc6150416644ea8b2c832105fc5c3b210e
Christoph Plate <christoph.plate@ovgu.de>
2025-04-16 20:11:22 +0200

    some more testing

--- test/runtests.jl
+++ test/runtests.jl
@@ -7,26 +7,31 @@ using Test
 
 @testset "Optimization.jl " begin
 
-    rosenbrock(x, p) = (p[1] - x[1])^2 + p[2] * (x[2] - x[1]^2)^2
-    x0 = zeros(2)
-    _p = [1.0, 1.0]
+    @testset "Rosenbrock" begin
+        rosenbrock(x, p) = (p[1] - x[1])^2 + p[2] * (x[2] - x[1]^2)^2
+        x0 = zeros(2)
+        _p = [1.0, 1.0]
 
-    @testset "With constraints" begin
         cons(res, x, p) = (res .= [x[1]^2 + x[2]^2, x[1] * x[2]])
         optprob_wcons = OptimizationFunction(rosenbrock, Optimization.AutoForwardDiff(), cons = cons)
         prob_wcons = OptimizationProblem(optprob_wcons, x0, _p, lcons = [-Inf, -1.0], ucons = [0.8, 2.0])
-        sol = solve(prob_wcons, BlockSQPOpt())
-        @test isapprox(sol.u, [0.751964453; 0.484303067])
-    end
+        sol_bsqp_wcons = solve(prob_wcons, BlockSQPOpt())
+        sol_ipopt_wcons = solve(prob_wcons, Ipopt.Optimizer())
 
-    @testset "Without constraints" begin
         optprob_wocons = OptimizationFunction(rosenbrock, Optimization.AutoForwardDiff())
         prob_wocons = OptimizationProblem(optprob_wocons, x0, _p)
-        sol = solve(prob_wocons, BlockSQPOpt())
-        @test isapprox(sol.u, ones(2))
-    end
+        sol_bsqp_wocons = solve(prob_wocons, BlockSQPOpt())
+        sol_ipopt_wocons = solve(prob_wocons, Ipopt.Optimizer())
+        @testset "Primal solution" begin
+            @test isapprox(sol_bsqp_wcons.u, sol_ipopt_wcons.u)
+            @test isapprox(sol_bsqp_wocons.u, sol_ipopt_wocons.u)
+        end
 
-    @testset "Lagrange multiplier" begin
+        @testset "Lagrange multiplier" begin
+            @test isapprox(sol_bsqp_wcons.original.multiplier, sol_ipopt_wcons.original.inner.mult_g)
+        end
+    end
+    @testset "LP on unit circle" begin
         lin_ex(x,p) = sum(x)
         function cons_unit(res,x,p)
             res .= sum(x.^2)
@@ -39,6 +44,12 @@ using Test
         sol_bsqp = solve(prob_lin, BlockSQPOpt())
         sol_ipopt = solve(prob_lin, Ipopt.Optimizer())
 
-        @test isapprox(abs.(sol_bsqp.original.multiplier), abs.(sol_ipopt.original.inner.mult_g))
+        @testset "Primal solution" begin
+            @test isapprox(sol_bsqp.u, sol_ipopt.u)
+        end
+        @testset "Lagrange multiplier" begin
+            @test isapprox(sol_bsqp.original.multiplier, sol_ipopt.original.inner.mult_g)
+        end
+
     end
 end
